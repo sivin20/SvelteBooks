47fc7997037e25b017803e8996ff36c3b61dd092
sivin20 <sivin20@student.sdu.dk>
2023-04-03 20:27:18 +0200

    Button color is white

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,7 @@
 
 <div class="flex">
     <input type="text" class="border border-solid rounded-lg border-slate-400 mr-2 p-2" bind:value={searchParam}>
-    <button on:click={searchBooks} class="bg-purple-400 rounded-lg p-2">Search</button>
+    <button on:click={searchBooks} class="bg-purple-400 rounded-lg p-2 text-white">Search</button>
 </div>
 <div><p>You searched for: {searchParam.replace(/\+/g, ' ')}</p></div>
 <div class="m-0 grid grid-cols-2">
