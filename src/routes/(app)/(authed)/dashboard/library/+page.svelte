66f3145ebf9f7948a6831664b4ed4eff8f90ecc1
sivin20 <sivin20@student.sdu.dk>
2023-06-05 14:24:18 +0200

    Adds filtering MY LIBRARIES on isbn

--- src/routes/(app)/(authed)/dashboard/library/+page.svelte
+++ src/routes/(app)/(authed)/dashboard/library/+page.svelte
@@ -8,6 +8,8 @@
     import {onMount} from "svelte";
     import LibraryCard from '$lib/components/library/libraryCard.svelte'
     import {SortService} from "$lib/services/sort.service";
+    import {faSearch, faXmark} from "@fortawesome/free-solid-svg-icons";
+    import Fa from "svelte-fa";
 
     /** @type {import('.$types').PageData} */
     export let data
@@ -21,9 +23,12 @@
     let bookToBeDeleted: Book;
 
     let books: Book[] = [];
+    let unfilteredBooks: Book[] = [];
+    $: console.log("Unf", unfilteredBooks)
 
-    let sortParam
-    let sortAscending: boolean
+    let sortParam: any = 'title'
+    let sortAscending: boolean = true
+    let isbnSearch: string
 
     const getBooksFromLibrary = async () => {
         return BookService.getBooksFromLibrary(current_library.id)
@@ -38,6 +43,28 @@
         SortService.sort(books, sortAscending ? sortArg : `-${sortArg}`)
         sortParam = sortArg
         books = books
+    }
+
+    function filterBooksOnISBN() {
+        if(!isbnSearch) {
+            toast.error(`Please add input before attempting filtering`, {
+                duration: 5000,
+                position: "top-right"
+            })
+        } else {
+            let b = books.filter(book => book.isbn_13 === isbnSearch)
+            if(!!b) {
+                unfilteredBooks = books
+                books = b
+            }
+        }
+    }
+
+    function resetBookFilters() {
+        books = unfilteredBooks;
+        unfilteredBooks = [];
+        unfilteredBooks = unfilteredBooks
+        isbnSearch = ''
     }
 
     async function handleGetBooks(library: Library) {
@@ -114,12 +141,30 @@
             <div class="filter-button flex items-center mr-4 justify-between"
                  on:click={() => {sortBooks('page_count')}}
                  class:active={sortParam === 'page_count'}>
-                <p>Pages</p>
+                <p>PAGES</p>
                 <svg xmlns="http://www.w3.org/2000/svg" width="9.33" height="6.02" viewBox="0 0 11.5 7.425" class:flipped={sortAscending && sortParam === 'page_count'}>
                     <path id="Icon_awesome-angle-up" data-name="Icon awesome-angle-up" d="M6.357,10.987l4.886,4.886a.859.859,0,0,1,0,1.218l-.812.812a.859.859,0,0,1-1.218,0l-3.467-3.46L2.283,17.906a.859.859,0,0,1-1.218,0L.25,17.094a.859.859,0,0,1,0-1.218l4.886-4.886a.86.86,0,0,1,1.221,0Z" transform="translate(11.496 18.16) rotate(180)"/>
                 </svg>
             </div>
-            <div></div>
+            <div class="flex justify-center items-center">
+                <div class="flex p-1 bg-[--input-field-color] rounded-s-[6px] w-full max-w-[650px] h-[32px] items-center">
+                    <Fa class="h-full mr-2 ml-2 text-[--secondary--accent-1]" icon="{faSearch}"></Fa>
+                    <input class="h-full w-full bg-transparent" type="text" id="bookSearch"
+                           placeholder="ISBN" bind:value={isbnSearch}
+                           disabled="{unfilteredBooks.length}">
+                </div>
+                <button class="small-secondary-button"
+                        style="border-bottom-left-radius: 0 !important;border-top-left-radius: 0 !important;"
+                        on:click="{(() => {filterBooksOnISBN()})}"
+                        disabled="{unfilteredBooks.length}">SEARCH</button>
+            </div>
+            {#if unfilteredBooks.length}
+                <div class="ml-4 flex justify-center items-center border-2 border-[--secondary--accent-2] rounded-2xl p-1 cursor-pointer"
+                     on:click={() => {resetBookFilters()}}>
+                    <p class="mr-2 p-0">{isbnSearch}</p>
+                    <Fa color="red" icon="{faXmark}"></Fa>
+                </div>
+            {/if}
         </div>
 
         <section class="mt-4 md:mt-0 flex flex-col items-center w-full">
