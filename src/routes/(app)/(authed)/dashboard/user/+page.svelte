89b83768a1bfb03ba813a0bac31f52bca14d9a7c
sivin20 <sivin20@student.sdu.dk>
2023-09-07 20:10:18 +0200

    Updates styling again for Changing user profile image
    - follows the design of how its done in Vercel settings

--- src/routes/(app)/(authed)/dashboard/user/+page.svelte
+++ src/routes/(app)/(authed)/dashboard/user/+page.svelte
@@ -28,27 +28,38 @@
         <p class="text-[50px]"><strong>USER SETTINGS</strong></p>
     </div>
     <section class="flex flex-row flex-wrap">
-        <div>
-            <p>Change user profile image</p>
-            <form action="?/avatar" method="post" enctype="multipart/form-data" class="flex items-center flex-col relative w-[150px]">
-                {#if avatar}
-                    <img class="h-[100px] w-[100px] rounded-full" src="{avatar}" alt="d" />
-                    <button on:click|preventDefault={() => {clearInputFile()}} class="cursor-pointer absolute right-5 top-0">
-                        <Fa color="red" icon="{faCircleXmark}"/>
-                    </button>
-                {:else}
-                    <img class="h-[100px] w-[100px]" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" alt="User profile" />
-                {/if}
-                <button class="flex items-center cursor-pointer flex-col mt-2" on:click|preventDefault={()=>{fileinput.click();}}>
-                    <img class="h-[40px] w-[40px] flex" src="https://static.thenounproject.com/png/625182-200.png" alt="upload image icon" />
-                    <div class="chan">Choose Image</div>
-                </button>
-                <input style="display:none"
-                       type="file"
-                       name="avatar"
-                       placeholder="avatar"
-                       accept="{authorizedExtensions.join(',')}" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
-                <button class="primary-button mt-4" type="submit" disabled="{!avatar}">SAVE</button>
+        <div class="border-2 border-[--input-field-color] w-1/2">
+            <form action="?/avatar" method="post" enctype="multipart/form-data" class="relative">
+                <div class="p-4 grid grid-cols-3">
+                    <div class="col-span-2">
+                        <p class="text-xl font-semibold">Change user profile image</p>
+                        <p class="pt-2">This is your avatar</p>
+                        <p class="pt-2">Click on the avatar to upload a custom one from your files.</p>
+                    </div>
+                    <div class="col-span-1 justify-self-end self-center relative">
+                        {#if avatar}
+                            <img class="h-[100px] w-[100px] rounded-full cursor-pointer" src="{avatar}"
+                                 alt="User avatar" on:click|preventDefault={()=>{fileinput.click();}}/>
+                            <button on:click|preventDefault={() => {clearInputFile()}} class="cursor-pointer absolute right-0 top-0">
+                                <Fa color="red" icon="{faCircleXmark}"/>
+                            </button>
+                        {:else}
+                            <img class="h-[100px] w-[100px] cursor-pointer" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png"
+                                 alt="User avatar" on:click|preventDefault={()=>{fileinput.click();}}/>
+                        {/if}
+                        <input style="display:none"
+                               type="file"
+                               name="avatar"
+                               placeholder="avatar"
+                               accept="{authorizedExtensions.join(',')}" on:change={(e)=>onFileSelected(e)} bind:this={fileinput}>
+                    </div>
+                </div>
+               <div class="border-t-2 border-[--input-field-color] grid grid-cols-3 items-center p-4 bg-[--input-field-color]">
+                   <p class="col-span-2">An avatar is optional, but is cool</p>
+                   <div class="w-[100px] flex justify-center col-span-1 justify-self-end">
+                       <button class="small-primary-button" type="submit" disabled="{!avatar}">SAVE</button>
+                   </div>
+               </div>
             </form>
         </div>
         <div class="ml-6 border-l-2 border-[--input-field-color] p-4">
