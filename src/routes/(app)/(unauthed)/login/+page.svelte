0133cc1d5e71af5c8a94ecab9b129fdd692874fb
sivin20 <sivin20@student.sdu.dk>
2023-09-30 13:31:33 +0200

    Adds mobile styling to login/register page

--- src/routes/(app)/(unauthed)/login/+page.svelte
+++ src/routes/(app)/(unauthed)/login/+page.svelte
@@ -26,12 +26,12 @@
     $: currentPageIsLogin
 </script>
 
-<main class="h-screen items-start justify-center flex relative overflow-hidden">
+<main class="h-screen items-start justify-center flex relative overflow-hidden w-full">
     {#if !!currentPageIsLogin}
-        <section class="items-center justify-center flex h-full absolute"
+        <section class="items-center justify-center flex h-full absolute w-full"
                  in:fly={{ y: '100%', duration: 1000, opacity: 1}}
                  out:fly={{ y: '100%', duration: 1000, opacity: 1}}>
-            <div class="bg-[--primary] w-[125px] h-full flex flex-col justify-between mb-[200px]">
+            <div class="bg-[--primary] sm:w-[75px] md:w-[125px] w-[50px] h-full flex flex-col justify-between mb-[200px]">
                 <div class="mt-10 h-full flex items-center justify-center">
                     <a href="/">
                         <img src="{logo}" alt="logo" class="h-[285px]">
@@ -39,11 +39,11 @@
                 </div>
                 <div class="arrow-up"></div>
             </div>
-            <div class="h-full relative ml-[100px] flex flex-col items-center justify-center w-[425px]"
+            <div class="h-full relative md:ml-[100px] flex flex-col items-center justify-center md:w-[425px] w-full"
                  in:fly={{ duration: 1000}}
                  out:fly={{duration: 1000}}>
                     <!--        Login-->
-                    <div class="flex flex-col items-center w-[425px]">
+                    <div class="flex flex-col items-center sm:w-[425px]">
                         <h1 class="text-[50px] mb-10 text-[--secondary] font-bold">LOGIN</h1>
                         <form action="?/login" method="POST" class="flex flex-col items-center w-full">
                             <div class="input-text">
@@ -55,29 +55,30 @@
                                 <input type="password" id="loginPassword" name="password">
                             </div>
 
-                            <button type="submit" class="secondary-button mt-5">LOGIN</button>
+                            <button type="submit" class="md:secondary-button small-secondary-button mt-5">LOGIN</button>
                         </form>
-                        <p class="mt-5 font-bold cursor-pointer"><u>Forgot password?</u></p>
+<!--                        <p class="mt-5 font-bold cursor-pointer"><u>Forgot password?</u></p>-->
                         <p class="mt-5 font-bold cursor-pointer" on:click={() => {change()}}><u>Don't have an account yet?</u></p>
                     </div>
             </div>
         </section>
     {:else}
-        <section class="items-center justify-center flex h-full absolute"
+        <section class="items-center justify-center flex h-full absolute w-full"
                  in:fly={{ y: '-100%', duration: 1000, opacity: 1}}
                  out:fly={{ y: '-100%', duration: 1000, opacity: 1}}>
-            <div class="bg-[--primary] w-[125px] h-full flex flex-col justify-between">
+            <div class="bg-[--primary] sm:w-[75px] md:w-[125px] w-[50px] h-full flex flex-col justify-between">
                 <div class="mt-10 h-full flex items-center justify-center">
                     <a href="/">
                         <img src="{logo}" alt="logo" class="h-[285px]">
                     </a>
                 </div>
+                <div class="arrow-up no-show"></div>
             </div>
-            <div class="h-full relative ml-[100px] flex flex-col items-center justify-center w-[425px]"
+            <div class="h-full relative md:ml-[100px] flex flex-col items-center justify-center md:w-[425px] w-full"
                  in:fly={{ duration: 1000}}
                  out:fly={{duration: 1000}}>
                 <!--        Register-->
-                <div class="flex flex-col items-center w-[425px]"
+                <div class="flex flex-col items-center sm:w-[425px]"
                      >
                     <h1 class="text-[50px] mb-10 text-[--secondary] font-bold">REGISTER</h1>
                     <form action="?/register" method="POST" class="flex flex-col items-center w-full">
@@ -101,9 +102,9 @@
                             <input type="password" id="registerPassword" name="password">
                         </div>
 
-                        <button type="submit" class="secondary-button mt-4">CREATE</button>
+                        <button type="submit" class="md:secondary-button small-secondary-button mt-4">CREATE</button>
                     </form>
-                    <p class="mt-5 font-bold cursor-pointer"><u>Forgot password?</u></p>
+<!--                    <p class="mt-5 font-bold cursor-pointer"><u>Forgot password?</u></p>-->
                     <p class="mt-5 font-bold cursor-pointer" on:click={() => {change()}}><u>Already have an account?</u></p>
                 </div>
             </div>
@@ -119,8 +120,27 @@
         height: 0;
         border-left: 60px solid transparent;
         border-right: 60px solid transparent;
-
         border-bottom: 40px solid #FFF;
+    }
+
+    .no-show {
+        border-bottom: 40px solid transparent !important;
+    }
+
+    @media only screen and (max-width:  768px) {
+        .arrow-up {
+            border-left: 37px solid transparent;
+            border-right: 37px solid transparent;
+            border-bottom: 30px solid #FFF;
+        }
+    }
+
+    @media only screen and (max-width: 640px) {
+        .arrow-up {
+            border-left: 25px solid transparent;
+            border-right: 25px solid transparent;
+            border-bottom: 20px solid #FFF;
+        }
     }
 
 </style>
