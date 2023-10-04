e1aba27181798248e8e28886426cb1719af48971
sigurd <sivin20@student.sdu.dk>
2023-10-04 10:53:08 +0200

    Adds temporary image to landing page (WIP)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 
 <script>
     import UnauthedHeader from '$lib/components/navbar/unauthedNavbar.svelte'
+    import LandingImage from '$lib/assets/woman_reading.jpeg'
+
     /** @type {import('.$types').PageData} */
     export let data;
 
@@ -12,17 +14,18 @@
     <div class="w-full">
         <UnauthedHeader/>
     </div>
-    <section class="w-full flex items-center p-10 bg-[--primary]">
+    <section class="w-full flex items-center justify-center p-10 bg-[--primary]">
         <div class="flex w-full max-w-[1400px]">
-            <div class="w-1/2 py-[64px]">
-                <p class="text-[50px]">Organize your reading life</p>
-                <p class="text-[25px]">All your books tracked in one place. Plans, reviews, and goals. Completely private.</p>
-                <div class="flex">
-                    <button class="primary-button">Get started</button>
+            <div class="w-1/2 py-[64px] flex flex-col justify-center">
+                <p class="text-[50px] font-bold">Organize</p>
+                <p class="text-[50px] text-white font-bold mt-[-20px]">your reading life</p>
+                <p class="text-[25px] text-white pt-4">All your books tracked in one place. Plans, reviews, and goals. Completely private.</p>
+                <div class="flex pt-8">
+                    <button class="secondary-button">Get started</button>
                 </div>
             </div>
-            <div class="w-1/2 py-[64px]">
-                image
+            <div class="w-1/2 py-[64px] flex items-center justify-center">
+                <img src="{LandingImage}" class="w-auto max-h-[400px]" alt="Woman reading a book in front of a bookshelf">
             </div>
         </div>
     </section>
